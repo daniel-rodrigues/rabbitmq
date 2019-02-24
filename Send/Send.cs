16b89e3a4f12065254c6305e30f9bf1f86a0b09b
Daniel Rodrigues <inf.daniel@gmail.com>
2019-02-24 01:33:33 -0300

    Implement Send App

--- Send/Send.cs
+++ Send/Send.cs
@@ -1,12 +1,39 @@
 ﻿using System;
-
+using RabbitMQ.Client;
+using System.Text;
 namespace Send
 {
-    class Program
+    class Send
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(
+                    queue: "hello",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null
+                    );
+
+                string message = "Hello World!!!";
+
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(
+                    exchange: "",
+                    routingKey: "hello",
+                    basicProperties:null,
+                    body: body
+                );
+
+                Console.WriteLine(" [X] Sent {0}",message);    
+            }
+
+            Console.WriteLine("Press [enter] to exit.");
+            Console.ReadLine();
         }
     }
 }
